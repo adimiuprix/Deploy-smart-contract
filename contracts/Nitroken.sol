16122b4c8ca23a424997afc3a6ab35e796bee253
nuknuki <tronvego@gmail.com>
2023-03-28 08:22:30 +0700

    Menambahkan Library @nomiclabs/hardhat-etherscan

--- contracts/Nitroken.sol
+++ contracts/Nitroken.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Defti is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("Defti", "DTI") {
+contract Nitroken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("Nitroken", "NTKN") {
         _mint(msg.sender, 1000000 * 10**decimals());
     }
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,23 @@
 // Kita memerlukan Runtime Environment
 // Library ini sudah cukup untuk compile, tadi nya menggunakan hardhat itu sendiri.
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");  // library ini untuk verify smart contract
 
 async function main() {
   // Get contract instance
-  const defti = await ethers.getContractFactory("Defti");
+  const token = await ethers.getContractFactory("Nitroken");
 
   // Deploy contract
-  const Defti = await defti.deploy();
+  const Nitroken = await token.deploy();
 
   // Print for aesthetics :D (Optional)
   console.log("Deploying, please wait...");
 
   // Wait for deployment to finish
-  await Defti.deployed();
+  await Nitroken.deployed();
 
   // Another aesthetics wkwkwk
-  console.log("Your contract is:", Defti.address);
+  console.log("Your contract is:", Nitroken.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
